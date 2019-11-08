Found the issue! I was normalizing the test data incorrectly. Benchmarking for CIFAR-100 and ImageNet-200 is underway, now building up the OmniArt pipeline.

hypersphere_classes_opt_sgd.py
@@ -1,12 +1,7 @@
 #
 # Obtain hyperspherical prototypes prior to network training.
 #
-# @inproceedings{mettes2016hyperspherical,
-#  title={Hyperspherical Prototype Networks},
-#  author={Mettes, Pascal and van der Pol, Elise and Snoek, Cees G M},
-#  booktitle={Advances in Neural Information Processing Systems},
-#  year={2019}
-# }
+# Portions of code taken from https://github.com/psmmettes/hpn/blob/master/helper.py
 #
 import os
 import sys
@@ -37,7 +32,7 @@ def prototype_loss(prototypes):
 #
 def create_hypersphere_loss_w_sgd(num_classes, output_dimension, unique_class_numbers):
     prototypes = torch.randn(num_classes, output_dimension)
-    prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1))
+    prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1), requires_grad = True)
     optimizer = optim.SGD([prototypes], lr=.01, momentum=.9)
 
     # Optimize for separation.
@@ -50,7 +45,7 @@ def create_hypersphere_loss_w_sgd(num_classes, output_dimension, unique_class_nu
         optimizer.step()
         print("Epoch {0}: {1}".format(epoch, sep))
         # Renormalize prototypes.
-        prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1))
+        prototypes = nn.Parameter(F.normalize(prototypes, p=2, dim=1), requires_grad = True)
         optimizer = optim.SGD([prototypes], lr=.01, momentum=.9)
 
     class_matched_points = dict(zip(unique_class_numbers, prototypes.detach().numpy()))

main_classification.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 
-output_dimension = 25
+output_dimension = 10
 privileged_info = None
 lr = .01
 momentum = .9
@@ -16,6 +16,7 @@ use_cuda = True
 batch_size = 128
 epochs = 300
 device = torch.device("cuda" if use_cuda else "cpu")
+dataset = 'cifar'
 
 train_loader, test_loader = cifar_loader.get_cifar_data(batch_size=batch_size)
 unique_classes = list(set(train_loader.dataset.classes)) #class names privileged information, do not use for now
@@ -31,7 +32,7 @@ class_matched_points = create_hypersphere_loss_w_constraints(num_classes = num_c
 # class_matched_points = create_hypersphere_loss_w_sgd(num_classes = num_classes,
 #                                                      output_dimension = output_dimension,
 #                                                      unique_class_numbers = unique_class_numbers)
-
+#
 
 model = resnet32(output_dimension).to(device)
 optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=1e-4)

nn_losses.py
@@ -2,7 +2,7 @@ import torch
 import scipy
 
 def classification_loss(hypersphere_prediction, hypersphere_labels):
-    cosine_similarity = (1 - torch.nn.functional.cosine_similarity(hypersphere_prediction, hypersphere_labels, dim=1, eps=1e-8)) ** 2
+    cosine_similarity = (1 - torch.nn.functional.cosine_similarity(hypersphere_prediction, hypersphere_labels, dim=1, eps=1e-8)).pow(2)
     sum_cosine_similarity = cosine_similarity.sum()
     return sum_cosine_similarity
 

train_and_test.py
@@ -5,12 +5,13 @@ from nn_losses import classification_loss
 import numpy as np
 import scipy
 import torch.nn.functional as F
+from sklearn.metrics.pairwise import cosine_similarity
 
-def train(model, device, train_loader, optimizer, class_matched_points, epoch):
+def train(model, device, train_loader, optimizer, epoch, classification_matched_points):
     model.train()
     for batch_idx, (local_batch, local_labels) in enumerate(train_loader):
         # Transfer to GPU
-        hypersphere_vector = torch.FloatTensor([list(class_matched_points[class_num.item()]) for class_num in local_labels]) #get the vector of the label
+        hypersphere_vector = torch.FloatTensor([list(classification_matched_points[class_num.item()]) for class_num in local_labels]) #get the vector of the label
         image, hypersphere_vector = local_batch.to(device), hypersphere_vector.to(device)
         optimizer.zero_grad()
         hypersphere_prediction = model(image)
@@ -21,7 +22,7 @@ def train(model, device, train_loader, optimizer, class_matched_points, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(image), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), cosine_loss.item()))
-            hypersphere_labels = assign_predicted_class(hypersphere_prediction.detach(), class_matched_points) #get closest matching prototypes
+            hypersphere_labels = assign_predicted_class(hypersphere_prediction.detach(), classification_matched_points) #get closest matching prototypes
             correct = (np.array(hypersphere_labels) == np.array(local_labels.cpu())) * 1
             print('Train Accuracy: ', correct.mean())
 
@@ -50,6 +51,17 @@ def assign_predicted_class(hypersphere_prediction, class_matched_points):
     hypersphere_label, hypersphere_vector = list(class_matched_points.keys()), np.array(list(class_matched_points.values()))
     predicted_label = []
     for prediction in hypersphere_prediction: #for every prediction, find the closest prototype and get the label
-        all_distances = [scipy.spatial.distance.cosine(prediction, label) for label in hypersphere_vector]
-        predicted_label.append(hypersphere_label[np.argmin(all_distances)])
+        all_distances_scipy = [1-scipy.spatial.distance.cosine(prediction, label) for label in hypersphere_vector] #1-distance gives similarity
+        predicted_label.append(hypersphere_label[np.argmax(all_distances_scipy)])
     return predicted_label
+
+
+def assign_regressed_value(hypersphere_prediction, upper_bound, lower_bound):
+    hypersphere_prediction = np.array(hypersphere_prediction.cpu())
+    hypersphere_label, hypersphere_vector = list(class_matched_points.keys()), np.array(list(class_matched_points.values()))
+    predicted_label = []
+    for prediction in hypersphere_prediction: #for every prediction, find the closest prototype and get the label
+        all_distances_scipy = [1-scipy.spatial.distance.cosine(prediction, label) for label in hypersphere_vector] #1-distance gives similarity
+        predicted_label.append(hypersphere_label[np.argmax(all_distances_scipy)])
+    return predicted_label
+